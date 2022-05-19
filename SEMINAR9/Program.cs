2527a31552c62f3a52e4fb6ae5519f32bead737b
Alex Green <a_green@inbox.ru>
2022-05-19 21:23:26 +0300

    seminar9 done

---
+++ SEMINAR9/Program.cs
@@ -0,0 +1,38 @@
+﻿/*NaturalNums(2, 8);
+
+void NaturalNums(int n, int m)
+{
+    if(n <= m)
+    {
+        NaturalNums(n, m - 1);
+        Console.Write(m + " ");
+    }
+}
+*/
+
+/*
+int Multiply(int a, int b)
+{
+    int result = 1;
+    if(b > 0)
+    {
+        result = a * Multiply(a, b - 1);
+    }
+
+    return result;
+}
+
+Console.Write("Введите число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+if (numB < 0)
+{    
+    Console.WriteLine("Введите положительноре число!");
+}
+else
+{
+ Console.Write(Multiply(numA, numB));   
+}
+*/
+
